b732e2992df3855d3b0384a6be0073df1a204474
abhiawatade <awatadeabhi@gmail.com>
2023-01-16 15:11:36 +0530

    server setup,middlewares,db connection

--- index.js
+++ index.js
@@ -2,11 +2,19 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 const { connectDB } = require("./config/db");
+const userRoutes = require("./routes/userRoutes");
+const playlistRoutes = require("./routes/playlistRoutes");
+const songRoutes = require("./routes/songRoutes");
 
 //middleware
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use("/api/user", userRoutes);
+app.use("/api/user/auth", playlistRoutes);
+// app.use("api/user/auth/playlists/:id", playlistRoutes);
+app.use("/api/user", songRoutes);
 
 app.listen(PORT, () => {
   console.log("server listening on port");
